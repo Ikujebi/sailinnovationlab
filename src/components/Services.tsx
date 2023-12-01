b7cfd5b79e971dcca18744d7dbaf731a403aefb2
ikujebikehinde <133238121+Ikujebi@users.noreply.github.com>
2023-12-01 15:51:45 +0100

    service

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,39 +1,43 @@
-import { Card } from "antd";
+interface CardData {
+  image: number;
+  title: string;
+  content: string;
+}
 
-const Services = () => {
-  const cardData = [
+const Services: React.FC = () => {
+  const cardData: CardData[] = [
     {
-        image: 1,
+      image: 1,
       title: "Coworking Space",
       content:
         "Empower young minds with a dynamic learning, exploration, and discovery environment",
     },
     {
-        image: 2,
+      image: 2,
       title: "Incubation Programs",
       content:
         "Fuel innovation in our community by providing a nurturing space for idea development and validation",
     },
     {
-        image: 3,
+      image: 3,
       title: "Tech Trainings",
       content:
         "Equip individuals with the essential skills needed to thrive in today's tech-driven world",
     },
     {
-        image: 4,
+      image: 4,
       title: "Social Programs",
       content:
         "Foster a sense of belonging through diverse social activities, creating a welcoming home-like environment",
     },
     {
-        image: 5,
+      image: 5,
       title: "Events and Community",
       content:
-        "Nurture connections and engagement with our events and Vibrant community",
+        "Nurture connections and engagement with our events and vibrant community",
     },
     {
-        image: 6,
+      image: 6,
       title: "Lifelong Learning",
       content:
         "Offer opportunities for continuous growth to small business owners, educators, and parents",
@@ -41,16 +45,34 @@ const Services = () => {
   ];
 
   return (
-    <div className="flex justify-center mt-[8rem]">
-      <header className=" flex flex-col justify-center  ">
-        <h1 className="bg-[#48c4b4] rounded-xl w-[20rem] text-white text-[1.1rem] flex justify-center">
-          Services
-        </h1>
-        <h2 className=" text-[2.8rem] mt-[1rem] font-black flex justify-center text-gray-800">
-          What We Offer
-        </h2>
-      </header>
-      <main></main>
+    <div>
+      <div className="flex flex-col justify-center mt-[8rem]">
+        <header className="flex flex-col justify-center">
+          <h1 className="bg-[#48c4b4] rounded-xl w-[20rem] mx-[auto] text-white text-[1.1rem] flex justify-center">
+            Services
+          </h1>
+          <h2 className="text-[2.8rem] mt-[1rem] font-black flex justify-center text-gray-800">
+            What We Offer
+          </h2>
+        </header>
+      </div>
+      <main className="md:w-[58rem] lg:w-[75rem] xl:w-[75rem] 2xl:w-[75rem] w-[21.5rem]    mx-auto ">
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3  gap-4 h-[55rem] md:h-[55rem] lg:h-[45rem] xl:h-[45rem] 2xl:h-[45rem]">
+          {cardData.map((card, index) => (
+            <article key={index} className="flex flex-col h-full ">
+              <div className="flex-grow border border-gray-300 p-4 rounded-xl ">
+                <div className="w-[5rem] h-[5rem] bg-blue-500 mb-4 mt-[2.5rem]"></div>
+                <h1 className="font-bold text-2xl leading-8 mb-2">
+                  {card.title}
+                </h1>
+                <p className="text-[1.01rem] md:text-[1.2rem]">
+                  {card.content}
+                </p>
+              </div>
+            </article>
+          ))}
+        </section>
+      </main>
     </div>
   );
 };
