42f75e47d805b00fdeb2bd392e5cd152d14441a3
ikujebikehinde <133238121+Ikujebi@users.noreply.github.com>
2024-01-10 17:36:52 +0100

    Update BlogNav.tsx

--- src/components/BlogNav.tsx
+++ src/components/BlogNav.tsx
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 
 const BlogNav: FC = () => {
   const readingBoy = "https://sailab.ng/wp-content/uploads/2023/10/Photo.png";
+  const circle = "https://sailab.ng/wp-content/uploads/2023/10/Image-16.png";
   return (
     <div>
       <header>
@@ -24,21 +25,41 @@ const BlogNav: FC = () => {
         </section>
         <section className="ml-[2rem] mt-[2rem] w-[27rem]">
           <article>
-            <img src={readingBoy} alt="" className="rounded-xl w-[24rem] ml-5" />
+            <img
+              src={readingBoy}
+              alt=""
+              className="rounded-xl w-[24rem] ml-5"
+            />
           </article>
           <article className="my-[2rem]">
-            <h5 className="text-[1.65rem] font-bold mb-3">SAIL Innovation Lab to drive youth Empowerment</h5>
+            <h5 className="text-[1.65rem] font-bold mb-3">
+              SAIL Innovation Lab to drive youth Empowerment
+            </h5>
             <p>
               SAIL Innovation Lab partnered with Co-creation Hub to develop a
               Community Innovation Space for young people in Lagos East
               Senatorial District.
             </p>
           </article>
+          <article className="mb-5 flex gap-3">
+            <button className="hover:text-white hover:bg-[#184c94] p-2 border-2 border-black hover:border-0 hover:p-3">
+              Read More
+            </button>
+            <div>
+              <img
+                src={circle}
+                alt=""
+                className="w-[3rem] h-[3rem] rounded-[50%]"
+              />
+             
+            </div>
+            <p>Sailab <span className="w-[0.3rem] h-[2rem] bg-gray-400 "></span> September 1, 2022</p>
+          </article>
         </section>
       </main>
 
       <footer>
-      <Footer/>
+        <Footer />
       </footer>
     </div>
   );
